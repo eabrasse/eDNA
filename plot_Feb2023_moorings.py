7a2c9cb10521216c1d3f399886397c1d70f0e8b4
eabrasse <ebrasseale@ucsd.edu>
2023-10-04 11:18:34 -0700

    typo

--- plot_Feb2023_moorings.py
+++ plot_Feb2023_moorings.py
@@ -138,8 +138,8 @@ with Image.open(figname) as img:
         axm.set_ylim([ymin,ymax])
         axm.set_ylabel('Part wt')
     
-    mooring_axes[0].text(0.9,0.9,'solid=time varying\ndashed=constant',transform=mooring_axes[0].transAxes,ha='right',va='top',fs=10)
-    ax_mean.text(0.9,0.9,'filled=time varying\nempty=constant',transform=ax_mean.transAxes,ha='right',va='top',fs=10)
+    mooring_axes[0].text(0.9,0.9,'solid=time varying\ndashed=constant',transform=mooring_axes[0].transAxes,ha='right',va='top',fontsize=10)
+    ax_mean.text(0.9,0.9,'filled=time varying\nempty=constant',transform=ax_mean.transAxes,ha='right',va='top',fontsize=10)
 
     fig.subplots_adjust(right=0.98,left=0.1,bottom=0.1,top = 0.98)
     outfn = home+'etools/plots/Feb2023_mooringsB.png'
