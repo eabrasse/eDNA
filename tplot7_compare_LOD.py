b9f7490da24e9a6dd77b4997ed6f16be0e479636
eabrasse <ebrasseale@ucsd.edu>
2023-10-19 16:42:12 -0700

    formatting

--- tplot7_compare_LOD.py
+++ tplot7_compare_LOD.py
@@ -181,7 +181,7 @@ for i in range(nDNA_conc_rel):
         ax.set_yticklabels([''])
     else:
         ax.set_ylabel('Dist from pen (m)')
-    ax.text(0.05,0.05,'DNA conc near source\n'+f'{int(DNA_conc_rel):}'+r' copies $\mu\mathrm{L}^{-1}$',transform=ax.transAxes,ha='left',va='bottom',bbox=props2)
+    ax.text(0.95,0.05,'DNA conc near source\n'+f'{int(DNA_conc_rel):}'+r' copies $\mu\mathrm{L}^{-1}$',transform=ax.transAxes,ha='right',va='bottom',bbox=props2)
 
 cbaxes = inset_axes(axs[-1], width="4%", height="60%", loc='center right',bbox_transform=axs[-1].transAxes,bbox_to_anchor=(0.15,0.,1,1))
 cb = fig.colorbar(p, cax=cbaxes, orientation='vertical')
